dcd31ed351e35aedc5d3fb5041e3cc884ebad982
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-02-11 09:05:34 +0300

    sprint9 commit1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,67 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
+
+@Slf4j
 @RestController
 public class FilmController {
+    private final HashMap<Integer, Film> filmsHashMap = new HashMap<>();
+    private int id = 0;
+
+    @ResponseBody
+    @PostMapping(value = "/filmHashMap")
+    public Film create(@Valid @RequestBody Film film) {
+        filmValidation(film);
+        filmsHashMap.put(film.getId(), film);
+        log.info("'{}' фильм добавлен в библиотеку с id '{}'", film.getName(), film.getId());
+        return film;
+    }
+
+    @ResponseBody
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        log.info("Количество фильмов в билиотеке '{}'", filmsHashMap.size());
+        return new ArrayList<>(filmsHashMap.values());
+    }
+
+    @ResponseBody
+    @PutMapping("/films")
+    public Film update(@Valid @RequestBody Film film) {
+        filmValidation(film);
+        if (filmsHashMap.containsKey(film.getId())) {
+            filmsHashMap.put(film.getId(), film);
+            log.info("'{}' фильм обновлен в библиотеке с id '{}'", film.getName(), film.getId());
+        } else {
+            throw new ValidationException("Попытка обновить несуществующий в библиотеке фильм");
+        }
+        return film;
+    }
+
+    private void filmValidation(Film film) {
+        if (film.getReleaseDate() == null ||
+                film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Неверная дата");
+        }
+        if (film.getName().isEmpty() || film.getName().isBlank()) {
+            throw new ValidationException("Попытка задать пустое имя фильму");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Попытка установить продолжительность меньше нуля");
+        }
+        if (film.getDescription().length() > 200 || film.getDescription().isEmpty()) {
+            throw new ValidationException("Описание бьльше 200 символов или пустое");
+        }
+        if (film.getId() <= 0) {
+            film.setId(++id);
+            log.info("У фильма установлен неверный id '{}", film.getId());
+        }
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.*;
+
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
+
+@Slf4j
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final HashMap<Integer, User> userHashMap = new HashMap<>();
+    private int id = 0;
+
+    @ResponseBody
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        userValidation(user);
+        userHashMap.put(user.getId(), user);
+        log.info("Пользователь '{}' сохранен с id '{}'", user.getEmail(), user.getId());
+        return user;
+    }
+
+    @ResponseBody
+    @GetMapping
+    public List<User> getUserHashMap() {
+        log.info("Количество пользователей '{}'", userHashMap.size());
+        return new ArrayList<>(userHashMap.values());
+    }
+
+    @ResponseBody
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        userValidation(user);
+        if (userHashMap.containsKey(user.getId())) {
+            userHashMap.put(user.getId(), user);
+            log.info("'{}' информация с id '{}' была обновлена", user.getLogin(), user.getId());
+        } else {
+            throw new ValidationException("Попытка обновления несуществующего пользователя");
+        }
+        return user;
+    }
+
+    private void userValidation(User user) {
+        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday() == null) {
+            throw new ValidationException("Неправильная дата рождения пользователя с id '" + user.getId() + "'");
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Неверный адрес электронной почты пользователя с id '" + user.getId() + "'");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+            log.info("Имя пользователя с id '{}' было установлено как '{}'", user.getId(), user.getName());
+        }
+        if (user.getId() == 0 || user.getId() < 0) {
+            user.setId(++id);
+            log.info("Неверный id пользователя был задан как '{}'", user.getId());
+        }
+        if (user.getLogin().isBlank() || user.getLogin().isEmpty()) {
+            throw new ValidationException("Неверный логин пользователя с id '" + user.getId() + "'");
+        }
+    }
+}
