ee7d8f89beef325eed6cd7d41597e509c6c98570
Dmitry <Mirokhin.Dmitry@yandex.ru>
2024-03-20 15:02:55 +0300

    ФЗ-11

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.genre;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.LinkedHashSet;
